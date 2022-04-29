a515ce67cb651f76cc6eff4326a6cf36b538a534
durian <huyk25sinh@gmail.com>
2022-04-29 13:24:02 +0700

    fix check_prime.py

--- check_prime.py
+++ check_prime.py
@@ -10,14 +10,19 @@ first_primes = [1,2,3,5,7,11,13,17,19]
 def check_prime(num):
     sqrt_num = sqrt(num)
     if num in first_primes: return True
+
     if str(sqrt_num).split(".")[1] == "0":
         return False
+
     else:
         if sqrt_num < 23:
             for i in first_primes:
-                if i<  sqrt_num and num//i == 0:
+                if i <  sqrt_num: 
+                    continue
+                elif num%i == 0:
                     return False
             return True
+
         else:
             for i in range(1,int(str(sqrt_num).split(".")[0])):
                 if check_prime(i) and num//i == 0:
@@ -25,6 +30,6 @@ def check_prime(num):
             return True
 
 if __name__ == "__main__":
-    print(check_prime(29))
+    print(check_prime(1039))
 
     
