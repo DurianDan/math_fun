bee73e4155d7931866a87e39f7613e582b6c12a0
Huy Nguyen Vu <huyk25sinh@gmail.com>
2023-09-29 23:05:09 +0700

    update: edge case: the list needs to contain the full category and subcategories names, to fully infer the naming scheme

--- self_made/name_scheme.py
+++ self_made/name_scheme.py
@@ -30,39 +30,54 @@ Solution:
 time: O(n!)
 space: O(1)
 '''
+from typing import List
 
-cate_scheme_name = [
+def category_scheme_name(schemes: List[str]) -> List[str]:
+
+    # sort the name => sub-categories are always after the bigger categories
+    schemes.sort()
+
+    # get the full scheme from categories' name suffix
+    for idx in range(len(schemes)-1):
+        
+        name = schemes[idx]
+        after_idx = idx+1
+
+        # loop stops, if the next category doesn't
+        #  starts with the now-category "name"  
+        while after_idx<len(schemes) and schemes[after_idx].startswith(name):
+            # turn string to list, to modify
+            array_char = list( schemes[after_idx] )
+
+            # change "-" to ">"
+            array_char[ len(name) ] = ">"
+            
+            # rejoin characters
+            schemes[after_idx] = "".join(array_char)
+            
+            # increment after_idx => to access the next category
+            after_idx+=1
+
+    return schemes
+
+if __name__=="__main__":
+    case = [
         "food",
         "food-ice-cream",
-        "food-grilled-steak",
         "food-ice-cream-vanilla",
+        "food-grilled-steak",
         "southest-asia",
         "southest-asia-vietnam",
         "southest-asia-vietnam-dak-lak"
     ]
 
-# sort the name => sub-categories are always after the bigger categories
-cate_scheme_name.sort()
+    edge_case_1 = case + [
+        "food-ice-cream-chocolate-proccessed",
+        "food-ice-cream-chocolate-raw",
+    ]
 
-# get the full scheme from categories' name suffix
-for idx in range(len(cate_scheme_name)-1):
-    
-    name = cate_scheme_name[idx]
-    after_idx = idx+1
+    edge_case_2 = edge_case_1 + ["food-ice-cream-chocolate"]
 
-    # loop stops, if the next category doesn't
-    #  starts with the now-category "name"  
-    while after_idx<len(cate_scheme_name) and cate_scheme_name[after_idx].startswith(name):
-        # turn string to list, to modify
-        array_char = list( cate_scheme_name[after_idx] )
-
-        # change "-" to ">"
-        array_char[ len(name) ] = ">"
-        
-        # rejoin characters
-        cate_scheme_name[after_idx] = "".join(array_char)
-        
-        # increment after_idx => to access the next category
-        after_idx+=1
-
-print(cate_scheme_name)
+    print("+ Categories naming schemes of `case`:\n" + str(category_scheme_name(case)))
+    print("+ Categories naming schemes of `edge_case_1`:\n" + str(category_scheme_name(edge_case_1)))
+    print("+ Categories naming schemes of `edge_case_2`:\n" + str(category_scheme_name(edge_case_2)))
