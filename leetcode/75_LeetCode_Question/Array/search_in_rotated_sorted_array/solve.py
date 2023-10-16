2102fd71fac5a3fba02b379e32ba94e4180b2a1e
Huy Vu Nguyen <huyk25sinh@gmail.com>
2023-10-16 09:12:52 +0000

    update: search_rotated_sorted_array with python and binary search

--- leetcode/75_LeetCode_Question/Array/search_in_rotated_sorted_array/solve.py
+++ leetcode/75_LeetCode_Question/Array/search_in_rotated_sorted_array/solve.py
@@ -2,4 +2,24 @@ def find_target_1(nums, target):
     try: return nums.index(target)
     except: return -1
 
-print(find_target_1([4,5,6,7,8,0,1,2], 6))
+def binary_search_rotated(nums, target):
+    r,l = len(nums) -1, 0
+    while l <= r:
+        mid = l + (r-l)//2
+        if nums[mid] == target: return mid
+        if nums[mid] >= nums[l]:
+            if target > nums[mid] or target < nums[l]:
+                l = mid + 1
+            else: 
+                r = mid - 1
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
+    return -1
+
+test_arr = [4,5,6,7,8,0,1,2]
+test_target = 1
+print(find_target_1(test_arr, test_target))
+print(binary_search_rotated(test_arr, test_target))
