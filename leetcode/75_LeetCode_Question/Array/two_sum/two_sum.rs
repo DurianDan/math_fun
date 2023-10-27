8c974c4a0924f419447ecbc164ba8644cb545eba
Huy Vu Nguyen <huyk25sinh@gmail.com>
2023-10-27 07:05:20 +0000

    update: solved twosum using hashmap in rust, understood more about rust HashMap and borrow checker

--- leetcode/75_LeetCode_Question/Array/two_sum/two_sum.rs
+++ leetcode/75_LeetCode_Question/Array/two_sum/two_sum.rs
@@ -1,4 +1,6 @@
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+use std::collections::HashMap;
+
+fn two_sum(nums: &Vec<i32>, target: i32) -> Vec<i32> {
     let nums_length: usize = nums.len();
     for (idx, num) in nums[..nums_length-1].iter().enumerate(){
         for next_idx in (idx+1)..nums_length{
@@ -12,13 +14,15 @@ fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     vec![]
 }
 
-fn twosum_hashmap(nums: &Vec<i32>, target: i32) -> Vec<i32>{
+fn two_sum_hashmap(nums: &Vec<i32>, target: i32) -> Vec<i32>{
     let mut table = HashMap::<i32, i32>::new();
     for (idx, num) in nums.iter().enumerate(){
-        match table.get(target-num) {
-            Some(remainer_idx) => return vec![idx, remainer_idx],
-            None => table.insert(num, idx)
-        }
+        match table.get(&(target-num)) {
+            Some(remainer_idx) => {
+                return vec![idx as i32, *remainer_idx]
+            },
+            None => {table.insert(*num, idx as i32)}
+        };
     }
     return vec![];
 }
@@ -27,6 +31,6 @@ fn main(){
     let test_num: i32 = 12;
     let test_vec = vec![1,2,3,4,5,6,7,8];
 
-    let result: Vec<i32> = two_sum(test_vec, test_num);
-    println!("{:?}", result);
+    println!("two_sum {:?}", two_sum(&test_vec, test_num));
+    println!("two_sum_hashmap {:?}", two_sum_hashmap(&test_vec, test_num));
 }
