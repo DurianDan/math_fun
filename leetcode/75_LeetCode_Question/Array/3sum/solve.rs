6269d2741d4d883f984fee04a1b7458adf457219
Huy Vu Nguyen <62023446+DurianDan@users.noreply.github.com>
2023-11-09 04:04:41 +0000

    update: solving 3sum in rust

--- leetcode/75_LeetCode_Question/Array/3sum/solve.rs
+++ leetcode/75_LeetCode_Question/Array/3sum/solve.rs
@@ -1,1 +1,7 @@
 # idea1: O(n^2) looping through the array, and also hashmap-ing
+use std::collections::HashMap;
+
+fn three_sum(nums: &Vec<i32>, target_sum: i32)-> Vec<i32>{
+    let mut table = HashMap::<i32, i32>::new();
+    
+}
